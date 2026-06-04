ca22cec69469bea4abb47ceed20467cac154b4f6
Zahra Alvandi <zalvandi39@gmail.com>
2026-06-04 17:05:59 +0800

    Initial commit: Setup project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
