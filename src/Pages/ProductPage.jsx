d5e9a2f9448ae56c965e1ada56fde1e6d3e16d2b
Zahra Alvandi <zalvandi39@gmail.com>
2026-06-13 21:56:13 +0800

    productPage style changed

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,4 +1,10 @@
 import { useParams } from "react-router-dom";
+import {
+  ChevronLeftIcon,
+  HeartIcon,
+  StarIcon,
+  MinusIcon,
+} from "@heroicons/react/24/outline";
 
 export default function ProductPage() {
   const { id } = useParams();
@@ -9,7 +15,7 @@ export default function ProductPage() {
       title: "Strawberry Cheesecake",
       price: "$5.49",
       img: "/images/cheesecake.webp",
-      desc: "Creamy cheesecake with fresh strawberries",
+      desc: "Creamy and smooth cheesecake on a buttery graham cracker crust, topped with sweet strawberry sauce. A timeless favorit!",
     },
     {
       id: 2,
@@ -25,7 +31,68 @@ export default function ProductPage() {
   if (!product) return <div>Product not found</div>;
 
   return (
-    <div className="container mx-auto px-4 py-10">
+    <div className="container mx-auto px-4 pb-20">
+      {/* icons */}
+      <svg className="hidden">
+        <symbol
+          id="RoseIcon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-rose-icon lucide-rose"
+        >
+          <path d="M17 10h-1a4 4 0 1 1 4-4v.534" />
+          <path d="M17 6h1a4 4 0 0 1 1.42 7.74l-2.29.87a6 6 0 0 1-5.339-10.68l2.069-1.31" />
+          <path d="M4.5 17c2.8-.5 4.4 0 5.5.8s1.8 2.2 2.3 3.7c-2 .4-3.5.4-4.8-.3-1.2-.6-2.3-1.9-3-4.2" />
+          <path d="M9.77 12C4 15 2 22 2 22" />
+          <circle cx="17" cy="8" r="2" />
+        </symbol>
+        <symbol
+          id="apple"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-apple-icon lucide-apple"
+        >
+          <path d="M12 6.528V3a1 1 0 0 1 1-1h0" />
+          <path d="M18.237 21A15 15 0 0 0 22 11a6 6 0 0 0-10-4.472A6 6 0 0 0 2 11a15.1 15.1 0 0 0 3.763 10 3 3 0 0 0 3.648.648 5.5 5.5 0 0 1 5.178 0A3 3 0 0 0 18.237 21" />
+        </symbol>
+        <symbol
+          id="heart-pulse"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-heart-pulse-icon lucide-heart-pulse"
+        >
+          <path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5" />
+          <path d="M3.22 13H9.5l.5-1 2 4.5 2-7 1.5 3.5h5.27" />
+        </symbol>
+        <symbol id="bag" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-handbag-icon lucide-handbag"><path d="M2.048 18.566A2 2 0 0 0 4 21h16a2 2 0 0 0 1.952-2.434l-2-9A2 2 0 0 0 18 8H6a2 2 0 0 0-1.952 1.566z"/><path d="M8 11V6a4 4 0 0 1 8 0v5"/></symbol>
+      </svg>
+      <div className="flex items-center justify-between py-5">
+        {/* title and back icon */}
+        <ChevronLeftIcon className="w-6 h-6 text-pink-700"></ChevronLeftIcon>
+        <span>Cheesecake</span>
+        <HeartIcon className="w-6 h-6 text-pink-700"></HeartIcon>
+      </div>
       <div className="grid md:grid-cols-2 gap-10">
         {/* Image */}
         <img
@@ -35,28 +102,67 @@ export default function ProductPage() {
 
         {/* Info */}
         <div>
-          <h1 className="text-3xl font-bold text-pink-700">{product.title}</h1>
-
-          <p className="text-gray-600 mt-3">{product.desc}</p>
-
-          <p className="text-2xl font-bold text-pink-600 mt-5">
+          <h1 className="text-2xl md:text-3xl font-bold text-pink-700">
+            {product.title}
+          </h1>
+          <p className="text-xl md:text-2xl font-semibold text-pink-600 mt-5">
             {product.price}
           </p>
+          <div className="flex items-center gap-2 my-5">
+            {/* point */}
+            <StarIcon className="w-5 h-5 text-yellow-300"></StarIcon>
+            <p className="text-sm">
+              4.8 <span className="text-gray-300">(128 reviews)</span>
+            </p>
+            <MinusIcon className="transform rotate-90 w-5 h-5 text-gray-300"></MinusIcon>
+            <span className="text-green-500 text-sm">In Stock</span>
+          </div>
+          <p className="text-gray-600 mt-3 text-sm">{product.desc}</p>
+
+          {/* about cake */}
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between gap-2 bg-pink-100/50 rounded-xl px-2 py-4">
+              <svg className="w-6 h-6 text-pink-600">
+                <use href="#RoseIcon"></use>
+              </svg>
+              <span className="text-sm">Eggless</span>
+            </div>
+            <div className="flex items-center justify-between gap-2 bg-pink-100/50 rounded-xl px-2 py-4">
+              <svg className="w-6 h-6 text-pink-600">
+                <use href="#apple"></use>
+              </svg>
+              <span className="text-sm">Fresh Ingredients</span>
+            </div>
+            <div className="flex items-center justify-between gap-2 bg-pink-100/50 rounded-xl px-2 py-4">
+              <svg className="w-6 h-6 text-pink-600">
+                <use href="#heart-pulse"></use>
+              </svg>
+              <span className="text-sm">No Preservatives</span>
+            </div>
+          </div>
+
+          {/* Quantity */}
 
           {/* Add to cart */}
           <button
             className="
+            flex items-center justify-center gap-3 w-full md:w-[50%]
             mt-6
             bg-pink-600
             text-white
             px-6
-            py-3
+            py-4
             rounded-xl
             hover:bg-pink-700
             transition
           "
           >
+            <svg className="w-6 h-6">
+              <use href="#bag"></use>
+            </svg>
             Add to Cart
+            <MinusIcon className="w-5 h-5 rotate-90 text-white"></MinusIcon>
+            <p>{product.price}</p>
           </button>
         </div>
       </div>
